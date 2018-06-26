cd2fb79e66697547f08d20aa46b8d7675b7114dc
julia sheremet <juliajessicasher@gmail.com>
2018-06-26 11:18:05 -0700

    Add client side routing to the homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header(){
   let headerStyles = {
@@ -11,6 +12,7 @@ function Header(){
   return(
     <div style={headerStyles} className={style.greenText}>
       <h1>Help Queue</h1>
+      <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
 
     </div>
 
